5298f04ba827c4d36b5ef204ee5db8a81a8013f9
Jarrett Ang <48117695+JarrettAng@users.noreply.github.com>
2020-08-02 21:33:20 +0900

    Imported drawings, implemented text tree, created scene control base

---
+++ Assets/Scripts/UI/ContinueButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButton : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Button continueButton = default;
+
+    [Header("Attributes")]
+    [SerializeField] private float cooldown = 0.1f;
+
+    // Used to prevent accidental tapping and skipping of dialogue
+    public void DisableButton() {
+        continueButton.enabled = false;
+        Invoke("EnableButton", cooldown);
+    }
+
+    private void EnableButton() {
+        continueButton.enabled = true;
+    }
+}
---
+++ Assets/Scripts/UI/DialogueDisplay.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Yarn.Unity;
+
+public class DialogueDisplay : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI nameText = default;
+    [SerializeField] private TextMeshProUGUI contentText = default;
+
+    public void DisplayContentText(string text) {
+        contentText.text = text;
+    }
+
+    [YarnCommand("DisplayName")]
+    public void DisplayName(string characterName) {
+        string nameToDisplay;
+        
+        switch(characterName) {
+            case "Grandfather":
+                nameToDisplay = "You";
+                break;
+            case "Daughter":
+                nameToDisplay = "Amanda, your daughter";
+                break;
+            case "Son":
+                nameToDisplay = "Ryan, your son";
+                break;
+            case "Grandson":
+                nameToDisplay = "Oscar, your grandson";
+                break;
+            case "None":
+                nameToDisplay = null;
+                break;
+            default:
+                nameToDisplay = characterName;
+                break;
+        }
+
+        DisplayNameText(nameToDisplay);
+    }
+
+    private void DisplayNameText(string name) {
+        if(name == null) {
+            if(nameText.gameObject.activeSelf == true) nameText.gameObject.SetActive(false);
+        } else {
+            if(nameText.gameObject.activeSelf == false) nameText.gameObject.SetActive(true);
+            nameText.text = name;
+        }
+
+    }
+}
